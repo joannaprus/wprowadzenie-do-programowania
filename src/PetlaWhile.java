50765c7f500b6584d3a929934fac9c623620bec2
joannaprus <joanna.m.prus@gmail.com>
2022-07-23 15:02:37 +0200

    petla while

---
+++ src/PetlaWhile.java
@@ -0,0 +1,16 @@
+public class PetlaWhile {
+    public static void main(String[] args){
+        int licznik = 0;
+        while(licznik <10){
+            licznik ++;
+            System.out.println(licznik);
+        }
+
+
+        do {
+            licznik ++;
+            System.out.println(licznik);
+
+        } while (licznik <10);
+    }
+}
