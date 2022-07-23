29794e9de4e810ff7b3bbb5a8a810473fa96b34f
joannaprus <joanna.m.prus@gmail.com>
2022-07-23 13:09:10 +0200

    dodałam kotek

--- src/ForLoop.java
+++ src/ForLoop.java
@@ -1,15 +1,12 @@
 public class ForLoop {
-    public static void main( String [] args){
-        int x = 3;
-        int y;
-        System.out.println(x++);
-        System.out.println(++x);
-        y = x++;
-        System.out.println(y);
-        y=++x;
-        System.out.println(y);
-        System.out.println(++y);
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
+            if (i == 5) {
+                System.out.println("kotek");
+            }
+        }
+
 
     }
-
 }
